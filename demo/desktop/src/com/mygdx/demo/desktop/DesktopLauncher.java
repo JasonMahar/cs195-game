1700172ab0ca35f7dc80434cf792e036266bcef0
nateusse <81440558+nateusse@users.noreply.github.com>
2022-03-07 18:47:08 -0800

    libGDX demo

---
+++ demo/desktop/src/com/mygdx/demo/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.demo.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.demo.Launcher;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Launcher(), config);
+	}
+}
