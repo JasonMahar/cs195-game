41b81ccd8401e620e89acb7206aa78583368086b
JasonMahar <jasonmahar@gmail.com>
2022-03-14 17:53:29 -0700

    basic libgdx client

---
+++ client/android/src/com/wickedgames/cs195game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.wickedgames.cs195game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.wickedgames.cs195game.CS195Game;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new CS195Game(), config);
+	}
+}
---
+++ client/html/src/com/wickedgames/cs195game/client/HtmlLauncher.java
@@ -0,0 +1,22 @@
+package com.wickedgames.cs195game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.wickedgames.cs195game.CS195Game;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                // Resizable application, uses available space in browser
+                return new GwtApplicationConfiguration(true);
+                // Fixed size application:
+                //return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new CS195Game();
+        }
+}
