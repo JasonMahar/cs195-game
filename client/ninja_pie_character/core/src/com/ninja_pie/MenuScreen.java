527a15183ec63349bdf534178accc7c7a4dc9b33
Jason Mahar <jmahar1@mail.ccsf.edu>
2022-03-23 02:03:44 -0700

    Update MenuScreen.java

--- client/ninja_pie_character/core/src/com/ninja_pie/MenuScreen.java
+++ client/ninja_pie_character/core/src/com/ninja_pie/MenuScreen.java
@@ -22,6 +22,13 @@ public class  MenuScreen extends InputAdapter implements Screen {
     float playerx = 0;
     float playery = 8;
 
+
+    static Texture upPlayer = new Texture("ninja cat/ninja cat/animations/slash/ninja-slash_04.png");
+    static Texture   downPlayer = new Texture("ninja cat/ninja cat/animations/pull/ninja-pull lever_09.png");
+    static Texture leftPlayer = new Texture("ninja cat/ninja cat/animations/slash/ninja-slash_02.png");
+    static Texture rightPlayer = new Texture("ninja cat/ninja cat/animations/die/ninja-die_18.png");
+    static Texture idlePlayer = new Texture("ninja cat/ninja cat/animations/die/ninja-die_18.png");
+
     @Override
     public void show() {
         player = new Texture("ninja cat/ninja cat/animations/run/ninja-run_06.png");
@@ -37,6 +44,8 @@ public class  MenuScreen extends InputAdapter implements Screen {
 
     @Override
     public void render(float delta) {
+		//System.out.println( "calling render. delta = " + delta );
+		
         Gdx.gl.glClearColor(1, 1, 1, 0);
         ScreenUtils.clear(1,1,1,0);
         batch.begin();
@@ -44,24 +53,29 @@ public class  MenuScreen extends InputAdapter implements Screen {
         batch.draw(player,playerx,playery);
         //batch.draw(ground, 100, 100);
         if(Gdx.input.isKeyPressed(Input.Keys.UP) && playery<500){
-            playery += Gdx.graphics.getDeltaTime()*speed;
-            player = new Texture("ninja cat/ninja cat/animations/slash/ninja-slash_04.png");
+            playery += delta*speed;
+            //player = new Texture("ninja cat/ninja cat/animations/slash/ninja-slash_04.png");
+			player = upPlayer;
         }
         if(Gdx.input.isKeyPressed(Input.Keys.DOWN)&& playery>0){
-            playery -= Gdx.graphics.getDeltaTime()*speed;
-            player = new Texture("ninja cat/ninja cat/animations/pull/ninja-pull lever_09.png");
+            playery -= delta*speed;
+            //player = new Texture("ninja cat/ninja cat/animations/pull/ninja-pull lever_09.png");
+			player = downPlayer;
         }
         if(Gdx.input.isKeyPressed(Input.Keys.LEFT)&& playerx>0){
-            playerx -= Gdx.graphics.getDeltaTime()*speed;
-            player = new Texture("ninja cat/ninja cat/animations/slash/ninja-slash_02.png");
+            playerx -= delta*speed;
+            //player = new Texture("ninja cat/ninja cat/animations/slash/ninja-slash_02.png");
+			player = leftPlayer;
         }
         if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)&& playerx<800){
-            playerx += Gdx.graphics.getDeltaTime()*speed;
-            player = new Texture("ninja cat/ninja cat/animations/die/ninja-die_18.png");
+            playerx += delta*speed;
+            //player = new Texture("ninja cat/ninja cat/animations/die/ninja-die_18.png");
+			player = rightPlayer;
         }
-        if(!Gdx.input.isKeyPressed(Input.Keys.ANY_KEY)){
-            player = new Texture("ninja cat/ninja cat/animations/die/ninja-die_18.png");
-        }
+        //if(!Gdx.input.isKeyPressed(Input.Keys.ANY_KEY)){
+            //player = new Texture("ninja cat/ninja cat/animations/die/ninja-die_18.png");
+		//	player = idlePlayer;
+        //}
         batch.end();
 
     }
