e0157c59fc5ddc815abc55bb880b92d9123e0d48
nateusse <81440558+nateusse@users.noreply.github.com>
2022-04-21 15:49:42 -0700

    Add files via upload
    no gradle

---
+++ core/src/com/game/entities/Pie.java
@@ -0,0 +1,30 @@
+package com.game.entities;
+
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.game.BaseActor;
+
+public class Pie extends BaseActor {
+
+    public Pie(float x, float y, Stage stage) {
+        super(x, y, stage);
+        float random = MathUtils.random(5);
+        loadTexture("pie.png");
+
+        addAction(Actions.delay(1));
+        addAction(Actions.after(Actions.fadeOut(0.2f)));
+        addAction(Actions.after(Actions.removeActor()));
+        addAction(Actions.forever(Actions.rotateBy(30 + random, 1)));
+
+        setSpeed(400);
+        setMaxSpeed(400);
+        setDeceleration(0);
+    }
+
+    public void act(float dt) {
+        super.act(dt);
+        applyPhysics(dt);
+        wrapAroundWorld();
+    }
+}
