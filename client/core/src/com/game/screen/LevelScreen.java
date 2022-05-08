5d0393782f1b3753bc3a2ffe5cb8ad84674db4be
nat <natyeusse@gmail.com>
2022-05-08 12:33:40 -0700

    nat and jason branch

--- client/core/src/com/game/screen/LevelScreen.java
+++ client/core/src/com/game/screen/LevelScreen.java
@@ -1,298 +1,215 @@
 package com.game.screen;
 
 
-import com.badlogic.gdx.Gdx;
+
 import com.badlogic.gdx.Input;
-import com.badlogic.gdx.Input.Keys;
-import com.badlogic.gdx.audio.Music;
-import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.maps.MapObject;
 import com.badlogic.gdx.maps.MapProperties;
-import com.badlogic.gdx.scenes.scene2d.Event;
-import com.badlogic.gdx.scenes.scene2d.EventListener;
-import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.game.*;
 import com.game.entities.*;
-//import com.game.entities.ui.*;
-import com.wickedgames.cs195.model.*;
-import com.wickedgames.cs195.transport.*;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Hashtable;
+import static com.badlogic.gdx.Gdx.input;
 
 
 public class LevelScreen extends BaseScreen {
 
-    private Ninja ninja;
-    private Hashtable<Integer,Opponent> opponents;
-    private boolean win;
-//    ArrayList<Pie> pies;
-    private float audioVolume;
-    private Sound waterDrop;
-    private Music instrumental;
-    private Music oceanSurf;
+    Hero hero;
 
-    private GameSessionInterface serverSession;
-	private GameInstance gameData;
-	private PlayerData	playerData = null;
-	
-	// when timeSinceLastServerUpdate > GameDesignVars.GAMEPLAY_TIME_BETWEEN_UPDATES, 
-	//		get update from server
-	private float timeSinceLastServerUpdate = GameDesignVars.GAMEPLAY_TIME_BETWEEN_UPDATES + 1.0f;	
-	
-    
-    public LevelScreen() {
-    	super();
-	}
-
-	public void initializeDataFromServer() {
-
-		opponents = new Hashtable<Integer, Opponent>();
-		
-// STUB:       	//        	serverSession = new GameSession();
-		serverSession = new STUB_GameSession();
-		gameData = serverSession.getGameData(STUB_GameSession.DEFAULT_GAME_ID);
-				
-	    // Create Ninja's and Opponents' data 
-	    for(PlayerData player: gameData.getAllPlayers()){
-	    	
-	    	// Create Ninja's data 
-	    	if( player.getPublicID() == Ninja.getPlayerID() ) {	
-	    		playerData = player;	
-	    		continue;
-	    	}
-
-	    	System.out.println("LevelScreen.initializeDataFromServer creating player: " + player);
-
-		    // Create an Opponent's data 
-	    	Opponent newOpponent = new Opponent(player.getX(), player.getY(), mainStage, player);
-	    	opponents.put( player.getPublicID(), newOpponent );
-	    	newOpponent.setNamePlate(player.getName());
-
-	        BaseActor space = new BaseActor(0, 0, mainStage);
-	    }
-	    
-//		sendAndReceiveServerUpdate();
-	    
-	}
-	
-	public void initialize() {
-
-		
-//        TilemapActor tilemapActor = new TilemapActor("map.tmx", mainStage);
-//		
-//        for (MapObject obj : tilemapActor.getTileList("Rock")) {
-//            MapProperties props = obj.getProperties();
-//            new Rock((float) props.get("x"), (float) props.get("y"), mainStage);
-//        }
+    Treasure treasure;
 
 
-        BaseActor space = new BaseActor(0, 0, mainStage);
-        space.loadTexture("grass.jpg");
-        space.setSize(800, 600);
-//        space.setVisible(false);
-        BaseActor.setWorldBounds(space);
+    int health;
+    int coins;
+    int arrows;
+    boolean gameOver;
 
-        ninja = new Ninja(400, 300, mainStage);
-
-
-		initializeDataFromServer();
-        
-        
-        win = false;
-
-// jason's comment: Nat appears to remove this code. 
-//        But I think we're going to need buttons in the UI in the future 
-//        (unless they're being handled in another class, like they probably should.)
-//        so leaving them here (but commented out) as an example for later.
-//        
-/*
-        // user interface code
-        Button.ButtonStyle buttonStyle = new Button.ButtonStyle();
+    Label healthLabel;
+    Label coinLabel;
+    Label arrowLabel;
+    Label messageLabel;
 
 
-        Button restartButton = new Button(buttonStyle);
-        restartButton.setColor(Color.CYAN);
+    public void initialize() {
 
-        restartButton.addListener(new EventListener() {
-            @Override
-            public boolean handle(Event e) {
-                if (!LevelScreen.this.isTouchDownEvent(e)) return false;
+        TilemapActor tma = new TilemapActor("map.tmx", mainStage);
 
-                instrumental.dispose();
-                oceanSurf.dispose();
 
-                NinjaPie.setActiveScreen(new LevelScreen());
-                return true;
-            }
-        });
 
-        Button.ButtonStyle buttonStyle2 = new Button.ButtonStyle();
 
-        Texture buttonTex2 = new Texture(Gdx.files.internal("audio.png"));
-        TextureRegion buttonRegion2 = new TextureRegion(buttonTex2);
-        buttonStyle2.up = new TextureRegionDrawable(buttonRegion2);
+        for (MapObject obj : tma.getTileList("Rock")) {
+            MapProperties props = obj.getProperties();
+            new Rock((float) props.get("x"), (float) props.get("y"), mainStage);
+        }
 
-        Button muteButton = new Button(buttonStyle2);
-        muteButton.setColor(Color.CYAN);
-        muteButton.setChecked( GameDesignVars.START_WITH_SOUND_ENABLED );
 
-        muteButton.addListener(new EventListener() {
-            @Override
-            public boolean handle(Event e) {
-                if (!LevelScreen.this.isTouchDownEvent(e)) return false;
 
-                audioVolume = 1 - audioVolume;
-                instrumental.setVolume(audioVolume);
-                oceanSurf.setVolume(audioVolume);
-//                muteButton.setChecked( !muteButton.isChecked() );
-                
-                return true;
-            }
-        });
+        for (MapObject obj : tma.getTileList("Flyer")) {
+            MapProperties props = obj.getProperties();
+            new Flyer((float) props.get("x"), (float) props.get("y"), mainStage);
+        }
 
-        uiTable.pad(10);
+        MapObject treasureTile = tma.getTileList("Treasure").get(0);
+        MapProperties treasureProps = treasureTile.getProperties();
+        treasure = new Treasure((float) treasureProps.get("x"), (float) treasureProps.get("y"), mainStage);
 
-        uiTable.add().expandX().expandY();
-        uiTable.add(restartButton).top();
-        uiTable.add(muteButton).top();
+
+
+
+
+        MapObject startPoint = tma.getRectangleList("start").get(0);
+        MapProperties startProps = startPoint.getProperties();
+        hero = new Hero((float) startProps.get("x"), (float) startProps.get("y"), mainStage);
+
+
+        // User Interface
+        health = 3;
+        coins = 5;
+        arrows = 3000;
+        gameOver = false;
+
+        healthLabel = new Label(" x " + health, BaseGame.labelStyle);
+        healthLabel.setColor(Color.PINK);
+
+        coinLabel = new Label(" x " + coins, BaseGame.labelStyle);
+        coinLabel.setColor(Color.GOLD);
+
+        arrowLabel = new Label(" x " + arrows, BaseGame.labelStyle);
+        arrowLabel.setColor(Color.TAN);
+
+        messageLabel = new Label("...", BaseGame.labelStyle);
+        messageLabel.setVisible(false);
+
+
+
+        BaseActor healthIcon = new BaseActor(0, 0, uiStage);
+        healthIcon.loadTexture("heart-icon.png");
+
+        BaseActor arrowIcon = new BaseActor(0, 0, uiStage);
+        arrowIcon.loadTexture("pie.png");
+
+        uiTable.pad(20);
+        uiTable.add(healthIcon);
+        uiTable.add(healthLabel);
+        uiTable.add().expandX();
+        uiTable.add(arrowIcon);
+        uiTable.add(arrowLabel);
         uiTable.row();
-*/
-        
-/*
-// TODO: waterDrop is currently not used. Maybe use it as sound when pies hit?
-        waterDrop = Gdx.audio.newSound(Gdx.files.internal("Water_Drop.ogg"));
-        
-        instrumental = Gdx.audio.newMusic(Gdx.files.internal("Master_of_the_Feast.ogg"));
-        oceanSurf = Gdx.audio.newMusic(Gdx.files.internal("Ocean_Waves.ogg"));
+        uiTable.add(messageLabel).colspan(8).expandX().expandY();
+        uiTable.row();
 
-        audioVolume = GameDesignVars.START_WITH_SOUND_ENABLED ? 1.00f : 0.0f;
-        instrumental.setVolume(audioVolume);
-        instrumental.setLooping(true);
-        instrumental.play();
-        oceanSurf.setLooping(true);
-        oceanSurf.setVolume(audioVolume);
-        oceanSurf.play();
-        */
     }
 
-	
+    // GAME LOOP
     public void update(float deltaTime) {
 
-    	// update player before sending its data to the server. 
-    	// there probably should be this call:
-    	// player.update();
-    	// but player.act(); is doing double duty and updating before this method is called.
-        
+        if (gameOver) return;
 
-        // TODO: pie.update() should actually be done inside player.update() or at least player.act();
-    	//		but currently Ninja does not have a pies attribute.
-    	//		So potentially every player's and opponent's pies are owned by the LevelScreen.
-    	//			Note if this remains true, then the pie data needs to be sent and received by server
-    	//			somewhere other than PlayerData. Maybe GameData?
-        /*
-         *		// Player's pies
-                for(Pie pie: pies){
+        // hero ans sword movement controls
+        if (hero.isVisible()) {
+            if (input.isKeyPressed(Input.Keys.LEFT)) hero.accelerateAtAngle(180);
+            if (input.isKeyPressed(Input.Keys.RIGHT)) hero.accelerateAtAngle(0);
+            if (input.isKeyPressed(Input.Keys.UP)) hero.accelerateAtAngle(90);
+            if (input.isKeyPressed(Input.Keys.DOWN)) hero.accelerateAtAngle(270);
+        }
 
-        			pie.update(deltaTime);
+        for (BaseActor solid : BaseActor.getList(mainStage, "com.game.entities.Solid")) {
+            hero.preventOverlap(solid);
+
+            for (BaseActor flyer : BaseActor.getList(mainStage, "com.game.entities.Flyer")) {
+                if (flyer.overlaps(solid)) {
+                    flyer.preventOverlap(solid);
+                    flyer.setMotionAngle(flyer.getMotionAngle() + 180);
                 }
-         */
-    	
-        for (BaseActor rockActor : BaseActor.getList(mainStage, "com.game.entities.Rock"))
-            ninja.preventOverlap(rockActor);
-
-        /*
-        for (BaseActor starfishActor : BaseActor.getList(mainStage, "com.game.entities.Starfish")) {
-            Starfish starfish = (Starfish) starfishActor;
-            if (ninja.overlaps(starfish) && !starfish.collected) {
-                starfish.collected = true;
-                waterDrop.play(audioVolume);
-                starfish.clearActions();
-                starfish.addAction(Actions.fadeOut(1));
-                starfish.addAction(Actions.after(Actions.removeActor()));
-
-                Whirlpool whirl = new Whirlpool(0, 0, mainStage);
-                whirl.centerAtActor(starfish);
-                whirl.setOpacity(0.25f);
             }
-            
         }
-        */
-            
-                    
-        // Update Opponents' data, positions & actions
-        for(PlayerData player: gameData.getAllPlayers()){
-        	
-        	if( player == playerData ) {	continue;	}
-        	
-//                    	
-//                    	opponent.update(deltaTime);
 
-// TODO: this should actually be inside the PlayerData's update()        
-        	
-//            	     // Opponents' pies
-//            	        for(Projectile pie: opponent.getAllProjectiles()){
-//	
-//            				pie.update(deltaTime);
-//            	        }
+        for (BaseActor flyer : BaseActor.getList(mainStage, "com.game.entities.Flyer")) {
+            if (hero.overlaps(flyer)) {
+                hero.preventOverlap(flyer);
+                flyer.setMotionAngle(flyer.getMotionAngle() + 180);
+                Vector2 heroPosition = new Vector2(hero.getX(), hero.getY());
+                Vector2 flyerPosition = new Vector2(flyer.getX(), flyer.getY());
+                Vector2 hitVector = heroPosition.sub(flyerPosition);
+                hero.setMotionAngle(hitVector.angleDeg());
+                hero.setSpeed(100);
+                health--;
+            }
+
         }
-        
 
-    	timeSinceLastServerUpdate += deltaTime;
-    	
-// TODO: server updates almost certainly need to be moved into their own asynchronous thread
-//	 this is just quick & dirty try 
-    	if( timeSinceLastServerUpdate > GameDesignVars.GAMEPLAY_TIME_BETWEEN_UPDATES) {
-    		
-    		sendAndReceiveServerUpdate();
-    		timeSinceLastServerUpdate = 0.0f;
-    	}
-    	
-    	
-    
-    } // END update()
+        for (BaseActor arrow : BaseActor.getList(mainStage, "com.game.entities.Arrow")) {
+            for (BaseActor flyer : BaseActor.getList(mainStage, "com.game.entities.Flyer")) {
+                if (arrow.overlaps(flyer)) {
+                    flyer.remove();
+                    arrow.remove();
+                    Coin coin = new Coin(0, 0, mainStage);
+                    coin.centerAtActor(flyer);
+                    Smoke smoke = new Smoke(0, 0, mainStage);
+                    smoke.centerAtActor(flyer);
+                }
+            }
 
-	// updating the server with the players recent data has the side effect 
-	//		of returning the current state of the game, including opponents' data
-	private void sendAndReceiveServerUpdate() {
+            for (BaseActor solid : BaseActor.getList(mainStage, "com.game.entities.Solid")) {
+                if (arrow.overlaps(solid)) {
+                    arrow.preventOverlap(solid);
+                    arrow.setSpeed(0);
+                    arrow.addAction(Actions.fadeOut(0.5f));
+                    arrow.addAction(Actions.after(Actions.removeActor()));
+                }
+            }
+        }
 
-		gameData = serverSession.updatePlayerData(playerData);
-		
 
-	    // Update Opponents' data, positions & actions
-		// 		This may be unnecessary and just automatically update itself since 
-		//		we're reusing the same PlayerData instances
-		// 	
-	    for(PlayerData player: gameData.getAllPlayers()){
-	    	
-	    	if( player.getPublicID() == Ninja.getPlayerID() ) {	continue;	}
 
-		    // Find and update Opponent's data 
-	    	Opponent opponent = opponents.get(player.getPublicID());
-	    	
-// STUB: since we're not receive real server data, let client update predictively 
-//
-//	    	opponent.setPlayerData( player );
-        	
-//	    	System.out.println("LevelScreen.sendAndReceiveServerUpdate updating player: " + player);
 
-	    }
-		
-	} 
+        if (hero.overlaps(treasure)) {
+            messageLabel.setText("You win!");
+            messageLabel.setColor(Color.LIME);
+            messageLabel.setFontScale(2);
+            messageLabel.setVisible(true);
+            treasure.remove();
+            gameOver = true;
+        }
 
-    public boolean keyDown(int keyCode) {
+        if (health <= 0) {
+            messageLabel.setText("Game over...");
+            messageLabel.setColor(Color.RED);
+            messageLabel.setFontScale(2);
+            messageLabel.setVisible(true);
+            hero.remove();
+            gameOver = true;
+        }
 
-        if (keyCode == Input.Keys.SPACE)
-            ninja.shoot();
-        
-        if (Gdx.input.isKeyPressed(Keys.ESCAPE))
-            Gdx.app.exit();
+        healthLabel.setText(" x " + health);
+        arrowLabel.setText(" x " + arrows);
+    }
+
+
+
+    public void shootArrow() {
+        if (arrows <= 0) return;
+        arrows--;
+        Arrow arrow = new Arrow(0, 0, mainStage);
+        arrow.centerAtActor(hero);
+        arrow.setRotation(hero.getMotionAngle());
+        arrow.setMotionAngle(hero.getMotionAngle());
+    }
+
+    public boolean keyDown(int keycode) {
+
+        if (gameOver) return false;
+
+
+
+        if (keycode == Input.Keys.SPACE) {
+            shootArrow();
+        }
+
 
         return false;
     }
-
 }
